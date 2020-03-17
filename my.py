350c19379f8e8d09055e7511c63c6627afd4e633
alex2011ua <60536573+alex2011ua@users.noreply.github.com>
2020-03-17 14:13:22 +0200

    2

--- my.py
+++ my.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from flask import render_template
+import time
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('hello.html')
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    return render_template('hello.html', time=time.asctime())
 
 if __name__ == '__main__':
     app.run()
